acb114aecffd50f8ac3a0b1085a01a6693378820
Efrain <jesusayllonroa@gmail.com>
2016-07-25 13:11:27 -0500

    Initial Commit

---
+++ APIPopulate/Photos.swift
@@ -0,0 +1,14 @@
+//
+//  Photos.swift
+//  APIPopulate
+//
+//  Created by Efrain Ayllon on 7/25/16.
+//  Copyright © 2016 Efrain Ayllon. All rights reserved.
+//
+
+import UIKit
+
+class Photos: NSObject {
+    var title :String!
+    var thumbnailURL :String!
+}
